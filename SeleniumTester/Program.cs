533364ba4a9dc4862209a3af657954b22ca454ee
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-06-10 22:36:54 -0400

    - Selenium login

--- SeleniumTester/Program.cs
+++ SeleniumTester/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumTester
 {
@@ -10,6 +13,38 @@ namespace SeleniumTester
     {
         static void Main(string[] args)
         {
+            var url = "https://login.yahoo.com/";
+            //var pass = "";
+            // Automated login pass xpath = "//*[@id='login - passwd']";//*[@id="login-passwd"]
+
+            var chromeDriver = new ChromeDriver();
+
+            chromeDriver.Navigate().GoToUrl(url);
+
+            chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+
+            //IWebElement emailBox = chromeDriver.FindElement(By.XPath("//*[@id='login-username']"));
+            //emailBox.SendKeys();
+
+            chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(90);
+
+            IWebElement nextBox = chromeDriver.FindElement(By.XPath("//input[@id='login-signin']"));
+            nextBox.Click();
+
+            //chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(90);
+
+            IWebElement passBox = chromeDriver.FindElement(By.Id("login-passwd"));
+            passBox.SendKeys("Iloverice5");
+
+            /*
+            chromeDriver.FindElement(By.XPath("//input[@id='login-signin']")).Click();
+
+            IWebElement passField = chromeDriver.FindElement(By.XPath("//*[@id='login - passwd']"));
+
+            
+
+            //chromeDriver.FindElement(By.XPath("//*[@id='login-signin']")).Click();
+            */
         }
     }
 }
